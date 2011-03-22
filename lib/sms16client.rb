12dd7ed95d22c104cd9dd2fcd5935a76f46cd657
Nikolay Burlov <kohgpat@gmail.com>
2011-03-22 05:06:12 -0400

    initial commit

---
+++ lib/sms16client.rb
@@ -0,0 +1,80 @@
+# encoding: UTF-8
+
+require 'rubygems'
+require 'builder'
+require 'net/http'
+require 'net/https'
+require 'uri'
+
+module Sms16Client
+	VERSION = '0.0.1'
+	
+	class Client
+		attr_accessor :login, :password, :query
+	
+		def initialize(login, password)
+			@login = login
+			@password = password
+			@query = String.new
+		end
+		
+		def get_balance
+			xml = Builder::XmlMarkup.new(:target => @query)
+
+			xml.request {
+				xml.security {
+					xml.login(:value => @login)
+					xml.password(:value => @password)
+				}
+			}
+			
+			return execute_query(@query, 'https://my.sms16.ru/xml/balance.php')
+		end
+		
+		def send_message(message, destination, source)
+			xml = Builder::XmlMarkup.new(:target => @query)
+
+			xml.request {
+				xml.message(:type => "sms") {
+					xml.sender source.to_s
+					xml.text message.to_s
+					xml.abonent(:phone => destination.to_s, :number_sms => "1")
+				}
+				xml.security {
+					xml.login(:value => @login)
+					xml.password(:value => @password)
+				}
+			}
+			
+			return execute_query(@query, 'https://my.sms16.ru/xml/')
+		end
+		
+		def get_message_state(message_id)
+			xml = Builder::XmlMarkup.new(:target => @query)
+			
+			xml.request {
+				xml.security {
+					xml.login(:value => @login)
+					xml.password(:value => @password)
+				}
+				xml.get_state {
+					xml.id_sms message_id.to_s
+				}
+			}
+			
+			return execute_query(@query, 'https://my.sms16.ru/xml/state.php')
+		end
+		
+		private
+		
+			def execute_query(query, uri)
+				url = URI.parse(uri)
+				headers = {"Content-Type" => "text/xml"}
+				
+				request = Net::HTTP.new(url.host, url.port)
+				request.use_ssl = true
+				
+				return request.post(url.path, query, headers).body
+			end
+	end
+end
